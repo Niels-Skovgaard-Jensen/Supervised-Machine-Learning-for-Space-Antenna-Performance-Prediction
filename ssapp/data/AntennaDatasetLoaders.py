d1a93051fbc9b8778973365ae1660a48bb3aca3d
Niels Jensen <skoogy7@gmail.com>
2022-03-14 14:19:45 +0100

    Implemented Complex Data representation

--- ssapp/data/AntennaDatasetLoaders.py
+++ ssapp/data/AntennaDatasetLoaders.py
@@ -1,40 +1,40 @@
-from cmath import log
 from torch.utils.data import Dataset
 from pathlib import Path
 import numpy as np
 import torch
 
 
-def get_raw_dataset_path(dataset_name):
+def get_raw_dataset_path(dataset_name: str):
 
     main_dir = Path().cwd().parents[1]
     data_dir = main_dir / 'data'
     subdict_dir = data_dir / 'raw'
     dataset_dir = subdict_dir / dataset_name
 
-    cut_folder = dataset_dir / 'cut_files'
-    log_file = dataset_dir / "log_files" / "lookup.log"
+    cut_dir = dataset_dir / 'cut_files'
+    log_dir = dataset_dir / "log_files" 
 
-    return cut_folder, log_file
+    return cut_dir, log_dir
+
+
+def gen_coords_from_header(V_INI, V_INC,V_NUM, ):
+
+    thetas = np.linspace(V_INI,V_INI+V_INC*(V_NUM-1),int(V_NUM))
+    phis = None
+    return thetas, phis
 
 
 class SingleCutDataset(Dataset):
     """Field Cut dataset"""
 
-    def __init__(self,cut):
+    def __init__(self,cut: int):
         """
         Args:
             cut (integer) : Integer name of file in directory
         """
         
         # Define data placement
-        main_dir = Path().cwd().parents[1]
-        data_dir = main_dir / 'data'
-        raw_dir = data_dir / 'raw'
-        
-        param_dir = raw_dir / 'PatchAntennaDataset1' /  'log_files'
-        cut_dir = raw_dir / 'PatchAntennaDataset1' /  'cut_files'
-
+        cut_dir, param_dir = get_raw_dataset_path('ReflectorAntennaSimpleDataset1')
         cut_file = cut_dir / (str(cut)+'.cut')
         param_file = param_dir / 'lookup.log'
         
@@ -44,7 +44,6 @@ class SingleCutDataset(Dataset):
         self.field_cut = np.genfromtxt(cut_file, skip_header=2,dtype = np.float32).T
         
         antenna_parameters = np.genfromtxt(param_file, skip_header=1,dtype = np.float32).T
-        #antenna_parameters = antenna_parameters[:,cut]
         
         self.parameters = torch.Tensor([np.append(antenna_parameters[1:,cut],theta) for theta in self.thetas])
 
@@ -80,9 +79,9 @@ class ReflectorCutDataset(Dataset):
         """
         self.cuts = cuts
         self.flatten_output = flatten_output
+
         # Define data placement
-        cut_dir = Path('AntennaDatasets/ReflectorAntennaSimpleDataset1/cut_files')
-        param_dir = Path('AntennaDatasets/ReflectorAntennaSimpleDataset1/log_files')
+        cut_dir, param_dir = get_raw_dataset_path('ReflectorAntennaSimpleDataset1')
         param_file = param_dir / 'lookup.log'
         
         self.antenna_parameters = np.genfromtxt(param_file, skip_header=1,skip_footer=2500-cuts,dtype = np.float32)
@@ -126,20 +125,23 @@ class ReflectorCutDatasetComplex(ReflectorCutDataset):
     def __init__(self, cuts = 2499,
                  flatten_output = False,
                  standardized_parameters = False,
-                 co_polar_only = False):
+                 co_polar_only = False,
+                 mag_phase_transform = False):
         """
         Args:
             cut (integer) : Integer name of file in directory
-            flatten_output (Boolean) : Flattens output into [cuts, 2002]
+            flatten_output (Boolean) : Flattens output into [cuts, 1001]
             magnitude_only : Only return magnitude of the complex dataset
             standardized_parameters : Standarizes the dataset for every parameter
             
         """
         super().__init__(cuts = cuts, flatten_output = flatten_output)
         self.co_polar_only = co_polar_only
+        self.mag_phase_transform = mag_phase_transform
+
         self.co_polar_complex = torch.view_as_complex(self.field_cut[:,:,0:2]).reshape(-1,1001,1)
         self.x_polar_complex = torch.view_as_complex(self.field_cut[:,:,2:4]).reshape(-1,1001,1)
-        self.field_cut = torch.cat((co_polar_complex,x_polar_complex),2)
+        self.field_cut = torch.cat((self.co_polar_complex,self.x_polar_complex),2)
         
     def __len__(self):
         return super().__len__()
@@ -149,62 +151,86 @@ class ReflectorCutDatasetComplex(ReflectorCutDataset):
             idx = idx.tolist()
         
         parameters = self.antenna_parameters[idx,:]
-        field_val = self.field_cut[idx,:,:]
+
         
-        return parameters, mag_val
+        if self.mag_phase_transform:
+            magnitude = torch.abs(self.field_cut[idx,:,:])
+            phase = torch.angle(self.field_cut[idx,:,:])
+            return parameters, magnitude, phase
+        else:
+            field_val = self.field_cut[idx,:,:]
+            return parameters, field_val
     
 
-    
-class ReflectorCutDatasetMagPhase(ReflectorCutDataset):
+
+class PatchAntennaDataset(Dataset):
     """Reflector Dataset
     To use for loading and parsing the ReflectorAntennaSimpleDataset1"""
 
-    def __init__(self, cuts = 2499,
+    def __init__(self, cuts = 343,
                  flatten_output = False,
                  standardized_parameters = False):
         """
         Args:
             cut (integer) : Integer name of file in directory
-            flatten_output (Boolean) : Flattens output into [cuts, 2002]
-            magnitude_only : Only return magnitude of the complex dataset
+            flatten_output (Boolean) : Flattens output into [cuts, 4004]
             standardized_parameters : Standarizes the dataset for every parameter
             
         """
-        super().__init__(cuts = cuts, flatten_output = flatten_output)
-        
-        real_co_polar = torch.tensor(self.field_cut[:,0,:])
-        imag_co_polar = torch.tensor(self.field_cut[:,1,:])
+        self.cuts = cuts
+        self.flatten_output = flatten_output
 
+        # Define data placement
+        cut_dir, param_dir = get_raw_dataset_path('PatchAntennaDataset1')
+        param_file = param_dir / 'lookup.log'
         
-        real_x_polar = torch.tensor(self.field_cut[:,2,:])
-        imag_x_polar = torch.tensor(self.field_cut[:,3,:])
+        self.antenna_parameters = np.genfromtxt(param_file, skip_header=1,skip_footer=343-cuts,dtype = np.float32)
+        self.antenna_parameters = self.antenna_parameters.reshape(cuts,4)[:,1:4]
+
+        ## Hardcoded fix, might want to automate it a little more
+    
+        file_to_open = cut_dir / '0.cut'
+        V_INI, V_INC, V_NUM, C, ICOMP, ICUT, NCOMP = np.genfromtxt(file_to_open, max_rows=1, skip_header=1)
+        cut_1 = np.loadtxt(file_to_open, skiprows=1, max_rows=V_NUM).reshape(1,V_NUM,1,4)
+        cut_2 = np.loadtxt(file_to_open, skiprows= V_NUM+2, max_rows=V_NUM).reshape(1,V_NUM,1,4)
+        cut_3 = np.loadtxt(file_to_open, skiprows= V_NUM+3, max_rows=V_NUM).reshape(1,V_NUM,1,4)
+
+        self.thetas = np.linspace(V_INI,V_INI+V_INC*(V_NUM-1),int(V_NUM))
+
+
+        for i in range(1,cuts):
+            file_to_open = cut_dir / (str(i)+'.cut')
+            cut_1 = np.loadtxt(file_to_open, skiprows=1, max_rows=V_NUM).reshape(1,V_NUM,1,4)
+            cut_2 = np.loadtxt(file_to_open, skiprows= V_NUM+2, max_rows=V_NUM).reshape(1,V_NUM,1,4)
+            cut_3 = np.loadtxt(file_to_open, skiprows= V_NUM+3, max_rows=V_NUM).reshape(1,V_NUM,1,4)
+
+
+            self.field_cut = np.append(self.field_cut,openFileData,axis = 0)
         
-        
-        
-        co_polar_mag = torch.sqrt(self.field_cut[:,0,:]**2 + self.field_cut[:,1,:]**2).reshape(-1,1,1001)
-        x_polar_mag = torch.sqrt(self.field_cut[:,2,:]**2 + self.field_cut[:,3,:]**2).reshape(-1,1,1001)
-        
-        #co_polar_phase = torc.angle
-        #x_polar_phase = 
-        
-        self.mag_val = np.append(co_polar_mag,x_polar_mag,axis=2)
-        self.phase_val = np.append(co_polar_phase, x_polar_phase)
+        ## Convert to tensors
+        self.field_cut = torch.tensor(self.field_cut)
+        self.antenna_parameter = torch.tensor(self.antenna_parameters)
+        ## Apply to device?
 
     def __len__(self):
-        return self.field_cut.shape[2]
+        return self.cuts
+    
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         
-        parameters = torch.tensor(self.antenna_parameters[1:4,idx])
+        parameters = torch.tensor(self.antenna_parameters[idx,:])
         
         
         if self.flatten_output:
-            field_val = torch.tensor(self.field_cut[:,:,idx]).flatten()
+            field_val = self.field_cut[idx,:,:].flatten()
         else:
-            field_val = torch.tensor(self.field_cut[:,:,idx])
+            field_val = self.field_cut[idx,:,:]
 
             
-        return parameters, mag_val, phase_val 
-    
+        return parameters, field_val
+
+    def get_coords(self):
+        # To Be Implemented
+        return True
